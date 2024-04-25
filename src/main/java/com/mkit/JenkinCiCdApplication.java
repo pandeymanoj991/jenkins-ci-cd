77a6966247005fe89c6f61731cd22c7ce32a0c7b
pandeymanoj991 <mktechm@gmail.com>
2024-04-25 11:01:09 +0530

    first commit

---
+++ src/main/java/com/mkit/JenkinCiCdApplication.java
@@ -0,0 +1,14 @@
+package com.mkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinCiCdApplication.class, args);
+	}
+
+
+}
---
+++ src/main/java/com/mkit/controller/ResourceController.java
@@ -0,0 +1,15 @@
+package com.mkit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+    @GetMapping("/hiJinkin")
+    public String m1(){
+
+    System.out.println("Welcome to jenkins");
+    return "Hello Welcome to jenkins ";
+    }
+}
